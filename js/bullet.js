0c07e6dff712388823beac1b384f19038974f9e5
Necromantic98 <michael.sutter@stud.hslu.ch>
2023-03-03 14:08:57 +0100

    Balls

---
+++ js/bullet.js
@@ -0,0 +1,14 @@
+class bullet {
+    constructor(x, y, orientation, spriteRef, trajectory, lifetime) {
+        this.x = x;
+        this.y = y;
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.orientation = orientation;
+        this.spriteRef = spriteRef;
+        this.trajectory = trajectory;
+        this.lifetime = lifetime;
+        this.framesAlive = 0;
+    }
+
+}
--- js/spriteSettings.js
+++ js/spriteSettings.js
@@ -2,3 +2,15 @@ export const CHALLENGER_SPRITE = {
     url: "",
     //frame coordinates
 };
+
+export const BULLET_SPRITE = {
+    url: "",
+    sizeX: 1,
+    sizeY: 1,
+    shape: BULLET_SHAPE.CIRCLE,
+}
+
+export const BULLET_SHAPE = {
+    CIRCLE: "circle",
+    RECTANGLE: "rectangle",
+}
