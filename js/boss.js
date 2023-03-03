607ce68e1d3744bf208dc1874bc787439f702f22
Zytronik <omar.oberholzer@gmail.com>
2023-03-03 15:47:52 +0100

    boss sprite

---
+++ js/boss.js
@@ -0,0 +1,12 @@
+import { BOSS_SPRITE } from "./spriteSettings.js";
+
+export class Boss {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.sprite = new Image()
+        this.sprite.src = BOSS_SPRITE.url;
+        this.size = BOSS_SPRITE.radius;
+        this.speed = 0.2;
+    }
+}
