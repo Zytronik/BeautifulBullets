21fb2aeec1e9a8c548f6b36f11eef32273783cc3
Necromantic98 <michael.sutter@stud.hslu.ch>
2023-03-08 23:52:05 +0100

    math stuff

--- P5 patterns/sketch.js
+++ P5 patterns/sketch.js
@@ -2,38 +2,63 @@ const bossPosX = 225;
 const bossPosY = 80;
 let pointsPos = [];
 let trajectory = [];
-let points = 100;
-let counter = 0;
+let points = 25;
+let counter = 0.2;
+let switcherino = true;
+let translation;
+let boolTrans = true;
 
 function setup() {
+  translation = 0;
   createCanvas(450, 600);
   background(220, 10);
   angleMode(DEGREES);
   pattern1();
-  setInterval(pattern1,1000)
+  setInterval(pattern1,200)
 }
 
 function draw() {
-  background(220);
-  circle(bossPosX, bossPosY, 30)
+  background(220, 80);
+  circle(bossPosX+translation, bossPosY, 30);
+  // circle(mouseX, mouseY, 30);
+  pattern3();
+  
   colorMode(RGB);
-  noLoop();
-  pattern2();
+  // noLoop();
 }
 
 function pattern1() {
   let borderWidth = 150;
   strokeWeight(5)
-  for(let i = 0; i <= points; i++) {
+  for(let i = 0; i < points; i++) {
     // pointsPos.push([(width-borderWidth*2)/points*i+borderWidth, sin(180/points*i)*20+100])
-    pointsPos.push([bossPosX, bossPosY]);
-    trajectory.push([sin(360/points*i), cos(360/points*i), i]);
+    pointsPos.push([bossPosX+translation, bossPosY]);
+    // pointsPos.push([mouseX, mouseY]);
+    trajectory.push([sin(360/(points+1)*i), cos(360/(points+1)*i), i, switcherino]);
     // trajectory[i][0] = sin(360/(pointsPos.length)%50*i-90);
     // trajectory[i][1] = cos(360/(pointsPos.length)%50*i-90);
     // point((width-borderWidth*2)/points*i+borderWidth, sin(180/points*i)*20+100);
-
+    
   }
-  
+  if(switcherino == true) {
+    switcherino = false;
+  } else {
+    switcherino = true;
+  }
+
+  // let steps = 10;
+  // if (boolTrans == true) {
+  //   translation += steps;
+  // } else {
+  //   translation -= steps
+  // }
+
+  // if(translation >= 100) {
+  //   boolTrans = false;
+  //   // console.log(boolTrans)
+  // } else if (translation <= -100) {
+  //   boolTrans = true;
+  // }
 }
 
 function pattern2() {
@@ -67,23 +92,24 @@ function pattern2() {
 }
 
 function pattern3() {
+  let border = 50;
+  // console.log(pointsPos.length)
   strokeWeight(5)
-  counter += 0.1;
+  counter += 0.2;
   // console.log(pointsPos);
   for(let i = 0; i <= pointsPos.length-1; i++) {
     // console.log(pointsPos[i][0])
     point(pointsPos[i][0]+trajectory[i][0], pointsPos[i][1]+trajectory[i][1]);
-    if (pointsPos[i][0] <= -50 || pointsPos[i][0] >= width+50 || pointsPos[i][1] <= -50 || pointsPos[i][1] >= height+50) {
+    if (pointsPos[i][0] <= -border || pointsPos[i][0] >= width+border || pointsPos[i][1] <= -border || pointsPos[i][1] >= height+border) {
       pointsPos.splice(i, 1);
       trajectory.splice(i, 1)
     } else {
       pointsPos[i][0] = pointsPos[i][0]+trajectory[i][0];
-      pointsPos[i][1] = pointsPos[i][1]+trajectory[i][1];
-      
-      // trajectory[i][1] = cos(360/pointsPos.length%50*i)
-      
+      pointsPos[i][1] = pointsPos[i][1]+trajectory[i][1];      
     }
-    if(trajectory[i][2]%2 == 1) {
+    // console.log(trajectory.length, i);
+
+    if(trajectory[i][3] == true) {
       trajectory[i][0] = sin(360/points*trajectory[i][2]+counter)
       trajectory[i][1] = cos(360/points*trajectory[i][2]+counter)
     } else {
@@ -93,6 +119,9 @@ function pattern3() {
     }
     // trajectory[i][0] = sin(360/pointsPos.length*i+i)
   }
-  counter%0.1;
-  
+  counter%0.2;
+}
+
+function patter4() {
+
 }
