e86bcc99bf7face8def6b73595c1f104489c8750
simitomorrow <89848971+simitomorrow@users.noreply.github.com>
2023-04-05 18:26:04 +0200

    tesma pixijs

--- testma.js
+++ testma.js
@@ -1,48 +1,84 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-
-var x = 120,
-    y = 120,
-    radius = 10,
-    innerRadius = 0.000,
-    radius = radius,
-    gradient,
-    lineGradient,
-    step = 0.01,
-    gradientPoints = [0, 1/3, 2/3, 3/3, 4/3, 5/3, 6/3, 7/3],
-    mainColor = 'rgba(255, 0, 0, 1)',
-    subColor = 'rgba(200, 123, 0, 0.3)',
-    black = 'rgba(0, 0, 0, 0.5)',
-    white = 'rgba(255, 255, 255, 0.5)',
-    rotation = 0,
-    rotationSteps = 0.01;
-
-
-function gameLoop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-    let xRot = Math.cos(rotation) - Math.sin(rotation);
-    let yRot = Math.sin(rotation) + Math.cos(rotation);
-    lineGradient = ctx.createLinearGradient(x - radius * xRot, y - radius * yRot, x + radius * xRot, y + radius * yRot);
-    lineGradient.addColorStop("0", white);
-    lineGradient.addColorStop("1.0", black);
-    ctx.strokeStyle = lineGradient;
-    ctx.lineWidth = 3;
-    ctx.stroke();
-    rotation += rotationSteps;
-
-    ctx.beginPath();
-    ctx.fillStyle = mainColor;
-    ctx.arc(x, y, radius, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.fillStyle = subColor;
-    ctx.arc(x + 13*radius, y, radius, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.stroke();
-
-    requestAnimationFrame(gameLoop);
-}
-gameLoop();
+const app = new PIXI.Application({
+    resizeTo: window,
+    autoDensity: true,
+    antialias: true,
+    autoStart: false,
+    backgroundColor: 0x333333,
+    resolution: window.devicePixelRatio
+  });
+  document.body.appendChild(app.view);
+  
+  const templateShape = new PIXI.Graphics()
+    .beginFill(0xffffff)
+    .lineStyle({ width: 1, color: 0x333333, alignment: 0 })
+    .drawCircle(0, 0, 20);
+  
+  const { width, height } = templateShape;
+  
+  // Draw the circle to the RenderTexture
+  const renderTexture = PIXI.RenderTexture.create({
+    width,
+    height,
+    multisample: PIXI.MSAA_QUALITY.HIGH,
+    resolution: window.devicePixelRatio
+  });
+  // With the existing renderer, render texture
+  // make sure to apply a transform Matrix
+  app.renderer.render(templateShape, {
+    renderTexture,
+    transform: new PIXI.Matrix(1, 0, 0, 1, width / 2, height / 2)
+  });
+  
+  // Required for MSAA, WebGL 2 only
+  app.renderer.framebuffer.blit();
+  
+  // Discard the original Graphics
+  templateShape.destroy(true);
+  
+  class Shape extends PIXI.Sprite {
+    speed = 0;
+  }
+  
+  const shapes = [];
+  for (let i = 0; i < 4000; i++) {
+    const shape = new Shape(renderTexture);
+    shapes[i] = shape;
+  
+    shape.anchor.set(0.5);
+    shape.speed = 1 + Math.random() * 1.2;
+    shape.position.x = app.screen.width * Math.random();
+    shape.position.y = app.screen.height * Math.random();
+  
+    shape.tint = parseInt(Math.floor(Math.random() * 16777215).toString(16), 16);
+  }
+  
+  const container = new PIXI.Container();
+  container.addChild(...shapes);
+  
+  const text = new PIXI.Text("", {
+    fill: "white",
+    fontWeight: "bold",
+    fontSize: 16
+  });
+  text.position.set(10);
+  app.stage.addChild(container, text);
+  
+  const redraw = () => {
+    let startTime = performance.now();
+    for (let i = 0; i < shapes.length; i++) {
+      const shape = shapes[i];
+      shape.position.x += shape.speed;
+      if (shape.position.x > app.screen.width + width) {
+        shape.position.x -= app.screen.width + width + width;
+      }
+    }
+    text.text =
+      "Prepared: " +
+      Math.round(performance.now() - startTime) +
+      "ms, Points: n/a";
+    app.render();
+  
+    requestAnimationFrame(redraw);
+  };
+  
+  requestAnimationFrame(redraw);
